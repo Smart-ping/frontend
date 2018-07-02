076bfa1307416c3635cc57dd5b1c9091a0ed8087
Антон Холодков <Anton.Kholodkov@icandeliver.ru>
2018-07-02 15:31:04 +0300

    Fix nuxt generate

--- store/index.js
+++ store/index.js
@@ -3,6 +3,8 @@ import cookie from 'cookie'
 export const actions = {
   nuxtServerInit({ dispatch }, app) {
     return new Promise((resolve, reject) => {
+      if (!app.req) 
+        return
       const cookies = cookie.parse(app.req.headers.cookie || '')
       if (cookies.hasOwnProperty('x-access-token')) {
         app.$axios.defaults.headers.common['x-access-token'] = cookies['x-access-token']
